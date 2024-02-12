9bad1ff24527b3201cc76ab8f13711d144b69870
satin <satin3737@gmail.com>
2024-02-12 22:27:57 +0200

    first commit

---
+++ styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import {colors} from './const';
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    }
+});
+
+export default styles;
